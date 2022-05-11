ada11be95360bcb53ffecdc79138d3765a56b6cd
Maxim Levinzon <maxlevi58@gmail.com>
2022-05-11 19:25:32 +0300

    modify init.sh

--- app/init.sh
+++ app/init.sh
@@ -13,5 +13,6 @@ cp gsm_essentials/chatscripts_gprs /etc/chatscripts/gprs
 cp gsm_essentials/ppp_gprs /etc/ppp/peers/gprs
 cp gsm_essentials/pppd_connect.service /etc/systemd/system/pppd_connect.service
 
+ln -s /etc/systemd/system/pppd_connect.service /etc/systemd/system/multi-user.target.wants/pppd_connect.service
 systemctl daemon-reload
 systemctl start pppd_connect.service
